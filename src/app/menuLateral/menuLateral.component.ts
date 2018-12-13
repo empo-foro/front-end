b57208e215345157842dc976e6bca5356144b9f4
Marc <colominassans@gmail.com>
2018-12-13 12:21:55 +0100

    he añadido recursos de bootstrap y un componente nuevo para empezar a hacer el menú lateral de la página

---
+++ src/app/menuLateral/menuLateral.component.ts
@@ -0,0 +1,17 @@
+import {Component} from "@angular/core";
+@Component({
+    selector:"menu-lateral",
+    templateUrl:"./menuLateral.component.html",
+    styleUrls:["./menuLateral.component.css"]
+})
+export class menuLateral{
+
+    asignaturas=[{
+        nombre:"M03"
+    },{
+        nombre:"M02"
+    },{
+        nombre:"M04"
+    }]
+
+}
