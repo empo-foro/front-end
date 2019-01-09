a096a8e83714d2123ab3bcd7d8bbc0889e3ef210
Pablo <pabloperal16@gmail.com>
2019-01-09 12:47:42 +0100

    creados todos los componentes de la mainPage, css vinculado con el sass y también el html con el css en el TypeScript.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { mainPage } from './mainPage/mainPage.component';
 import { menuLateral } from './menuLateral/menuLateral.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    menuLateral
+    menuLateral,
+    mainPage
   ],
   imports: [
     BrowserModule
---
+++ src/app/mainPage/mainPage.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+@Component({
+    selector:"main-page",
+    templateUrl:"./mainPage.component.html",
+    styleUrls:["./mainPage.component.css"]
+})
+
+export class mainPage {
+
+}
