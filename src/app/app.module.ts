b9f1eb0eacadbaff6344c88fcd664e172c15940c
Pablo <pabloperal16@gmail.com>
2019-02-11 12:30:38 +0100

    creado el registro de los usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { mainPage } from './mainPage/mainPage.component';
 import { menuLateral } from './menuLateral/menuLateral.component';
+import { registrarUsuario } from "./registrarUsuario/registroUsuario.component";
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
@@ -15,7 +16,8 @@ import { CursosComponentComponent } from './cursos-component/cursos-component.co
     AppComponent,
     menuLateral,
     mainPage,
-    CursosComponentComponent
+    CursosComponentComponent,
+    registrarUsuario
   ],
   imports: [
     BrowserModule,
---
+++ src/app/registrarUsuario/registroUsuario.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+@Component({
+    selector:"registrar-usuario",
+    templateUrl:"./registrarUsuario.component.html",
+    styleUrls:["./registrarUsuario.component.css"]
+})
+export class registrarUsuario{
+
+}
