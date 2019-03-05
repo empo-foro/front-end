65457d41f390a0a3e3dba8bc7907e99e184898ea
Pablo <pabloperal16@gmail.com>
2019-03-05 13:07:22 +0100

    getUsers/addUsers/updateUsers/deleteUsrers

--- src/app/model/user.model.ts
+++ src/app/model/user.model.ts
@@ -1,10 +1,21 @@
 export class User {
-  id: number;
-  nif: string;
-  nombre: string;
-  password: string;
+  id: number = 0;
+  nif: string ="";
+  nombre: string = "";
+  password: string =  "";
   img: string;
-  tipo: string;
-  email: string;
-  biografia: string;
+  tipo: string = "";
+  email: string = "";
+  biografia: string = "";
+
+  constructor(id:number, nif:string, nombre:string, password:string, img:string, tipo:string, email:string,biografia:string) {
+    this.id=id;
+    this.nif=nif;
+    this.nombre=nombre;
+    this.password=password;
+    this.img=img;
+    this.tipo=tipo;
+    this.email=email;
+    this.biografia=biografia;
+  }
 }
---
+++ src/app/model/userSer.model.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@angular/core";
+import { HttpClient , HttpHeaders} from "@angular/common/http";
+import { Observable } from "rxjs";
+import { User } from "./user.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class userSer {
+    constructor(private http:HttpClient){}
+
+    getUsers() {
+        let url = "/EMPO/?controller=userclass";
+        return this.http.get(url,{headers: new HttpHeaders({'Content-Type':'application/json'})});
+    }
+
+    getUser(numUser:number) {
+        let url = "/EMPO/?controller=userclass&id="+numUser;
+        return this.http.get(url,{headers: new HttpHeaders({'Content-Type':'application/json'})});
+    }
+
+    addUser(user:User) {
+        let url = "/EMPO/?controller=userclass";
+        return this.http.post(url,user,{headers:new HttpHeaders({'Content-Type':'application/json'})});
+    }
+
+    updateUser(numUser:number, user:User) {
+        let url = "/EMPO/?controller=userclass&id="+numUser;
+        return this.http.put(url,user,{headers: new HttpHeaders({'Content-Type':'application/json'})});
+    }
+
+    deleteUser(numUser:number) {
+        let url = "/EMPO/?controller=userclass&id="+numUser;
+        return this.http.delete(url,{headers:new HttpHeaders({'Content-Type':'application/json'})});
+    }
+}
