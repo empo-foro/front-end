de65f17f048e48a3b01b3da565676501bf0a5e04
Pablo <pabloperal16@gmail.com>
2019-03-21 13:08:18 +0100

    Borrado el registrar centro ya que es inecesario, todos los centrios seran tipo=1, ya que solo esta STUCOM

--- src/app/registrarCentro/registrarCentro.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-@Component({
-    selector:'registrar-centro',
-    templateUrl:'./registrarCentro.component.html',
-    styleUrls:['./registrarCentro.component.css']
-})
-export class registrarCentro{
-
-}
